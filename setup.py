d77817328185af5576370a996d5493a9c899ffb3
Dmitry Shevelev <estranium@gmail.com>
2016-12-17 18:39:35 +0300

    Add url to setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='file_deleter',
       version='0.0.1',
       description='Delete all files that match pattern and was created before timepoint.',
-      url='',
+      url='https://github.com/estranium/file_deleter',
       author='Dmitry Shevelev',
       author_email='estranium@gmail.com',
       license='MIT',
